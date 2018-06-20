ddb720afaa6558b93190e233e4d6a829ae15ab8e
Alexander <nort3d@ya.ru>
2018-06-20 18:08:07 +0300

    Тестовое задание. Первый этап. Вывод текста "Здесь был Вася!".

---
+++ vandals/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace vandals.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Messege = "Здесь был Вася!";
+            return View();
+        }
+    }
+}
